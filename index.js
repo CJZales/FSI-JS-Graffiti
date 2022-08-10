fae67eaaac5f5054ddff588e6688a062fc7060b5
CJ <cjdegonzales@gmail.com>
2022-08-09 19:19:05 -0700

    red and left

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let header = document.querySelector('#page-header')
-header.style.textAlign = "left"
+header.style.textAlign = 'left'
 
 let dogImages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
@@ -7,10 +7,10 @@ for(let i = 0; i < dogImages.length; i++){
 }
 
 let dogName = document.querySelectorAll('.dog-name')
-h1.style.textAlign = "left"
+h1.style.textAlign = 'left'
 
 
 let fooTer = document.querySelectorAll('.footer')
-fooTer.style.color = "red"
+footer.style.color = 'red'
 
 // Select some elements...
